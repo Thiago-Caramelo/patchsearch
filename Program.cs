6a572bf6a136e6a2132cccb79a5931cc92a585c3
Thiago Caramelo <Thiago-Caramelo@users.noreply.github.com>
2016-09-29 17:41:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PatchSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return;
+            }
+
+            // obter todos os arquivos da pasta
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            foreach (var item in files)
+            {
+                using (StreamReader reader = new StreamReader(item))
+                {
+                    string fileContent = reader.ReadToEnd();
+
+                    int qt = 0;
+
+                    foreach (var term in args)
+                    {
+                        if (fileContent.Contains(term))
+                        {
+                            qt++;
+                        }
+                    }
+
+                    if (qt == args.Length)
+                    {
+                        Console.WriteLine(Path.GetFileName(item));
+                    }
+                }
+            }
+        }
+    }
+}
